1f326c271f96ecc4b5ce3d813c8f464c030e7498
student-07 <student-07>
2017-01-30 15:48:30 -0800

    jan30-js-variables

---
+++ jan30-js-variables/js/script.js
@@ -0,0 +1,27 @@
+// alert('This is an alert');
+
+/*
+  comment
+  out
+  multiple
+  lines
+*/
+
+var myNumberVar = 10, // Number Object
+    myFloatVar = 5.2, // Number Object
+    myStringVar = "Micaela Nienaber", // String Object
+    myBooleanVar = true, // Boolean Object
+    myArrayVar = ['one', 'stingray', 'banana', 8, true], //Object Object
+    myObjectVar = {
+      make: 'Envoy',
+      model: 'GMC',
+      color: 'white',
+      year: 2002
+    }; // Object Object
+
+console.log(typeof myNumberVar);
+console.log(typeof myFloatVar);
+console.log(typeof myStringVar);
+console.log(typeof myBooleanVar);
+console.log(typeof myArrayVar);
+console.log(typeof myObjectVar.model);
