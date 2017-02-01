0bc366bbe5fbdfc2ec6c92ae899b55e4bb69e81a
student-07 <student-07>
2017-02-01 15:53:16 -0800

    feb01-js-math

---
+++ feb01-js-math/js/script.js
@@ -0,0 +1,79 @@
+/*
+  6 Object types
+
+  Object Object
+  Array Object
+  String Object
+  Number Object
+  Float Object
+  Boolean Object
+*/
+
+var myStringVar = "Micaela Nienaber",
+    myNumberVar = 10,
+    myFloatVar = 5.2,
+    myArrayVar = ["cat", "dog", "fish", 3],
+    myBooleanVar = false,
+    myObjectVar = {
+      make: 'Envoy',
+      model: 'GMC',
+      color: 'white',
+      year: 2002
+    };
+
+var strOne = 'this is a string',
+    strTwo = "this is a string",
+    strThree = 'is this a string";',
+    strFour = 'this is david\'s basketball',
+    strFive = "Eric said: \"Here hav David's Corn nuts\"";
+
+var numOne = 1,
+    numTwo = '2'; // is a string because it's in quotes
+
+var fltOne = 5.7,
+    fltTwo = 0.00000000000;
+
+var boolOne = true,
+    boolTwo = false;
+
+// boolTwo === 0
+
+var arrOne = ['one', 'two', 'three'],
+    arrTwo = [1,2,3],
+    arrThree = [false, 1, 'two', 3];
+
+var objOne = {
+  one: 'blue',
+  two: 'orange',
+  three: 'pink'
+}
+
+var numNum = 6,
+    numNumdos = 4,
+    numNumone = 1,
+    numNumtres = 3,
+    equalModules = (numNum % numNumdos),
+    equalAdded = (numNum + numNumdos),
+    equalSubtracted = (numNum - numNumdos),
+    equalMultiplied = (numNum * numNumdos),
+    equalDivided = (numNum / numNumdos),
+    equalDecimal = (numNumone / numNumtres);
+
+
+console.log (equalAdded);
+console.log (equalSubtracted);
+console.log (equalMultiplied);
+console.log (equalDivided);
+console.log (equalDecimal);
+console.log (equalModules);
+
+// = is an assignment operator
+// NaN is Not a Number
+
+/*
+
+  *Create Two new variables
+  *Add the two variables together and assign sum to a new variable
+  *Console result of sum
+
+*/
