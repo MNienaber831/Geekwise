4388ae00a29358974d59d18f7ba6c8d927216ccf
student-07 <student-07>
2017-03-31 15:47:02 -0700

    mar31-js-themoviedb

---
+++ mar31-js-themoviedb/js/script.js
@@ -0,0 +1,89 @@
+(function(){
+  getReq("env.json", setVars);
+}());
+
+function getReq(url, cb){
+  var req = new XMLHttpRequest();
+  req.open('GET', url);
+  req.onload = function(){
+    if(req.readyState === 4 && req.status === 200){
+      cb( JSON.parse(req.responseText) );
+    }else{
+      console.log('error', req.statusText);
+    }
+  };
+  req.send(null);
+}
+
+var myVars;
+
+function setVars(data){
+  myVars = envVars();
+  console.log(myVars.get_url());
+
+  myVars.set_api_key(data.api_key);
+  console.log(myVars.get_api_key());
+
+  myVars.set_read_access(data.read_access);
+  console.log(myVars.get_read_access());
+
+  getMovies();
+
+}
+
+function getMovies(){
+  '&language=en-US&query=godzilla&page=1'
+  var url = myVars.get_url()+'3/search/movie?api_key='+myVars.get_api_key()+'&language=en-US&page=1&query=indiana';
+  getReq(url, parseMovies);
+  console.log(url);
+}
+
+function parseMovies(movies){
+  console.log(movies);
+  var main = document.querySelector('main');
+
+  movies.results.forEach(function(e, i){
+    var section = document.createElement('main'),
+        poster_path = document.createElement('img'),
+        title = document.createElement('h1'),
+        tagline = document.createElement('h3'),
+        overview = document.createElement('p'),
+        homepage = document.createElement('a');
+
+    poster_path.src = "http://image.tmdb.org/t/p/w500"+e.poster_path;
+    title.textContent = e.title;
+    tagline.textContent = e.tagline;
+    overview.textContent = e.overview;
+    homepage.href = e.homepage;
+    homepage.textContent = 'Read More';
+
+    section.append(poster_path, title, tagline, overview, homepage);
+    main.appendChild(section);
+  });
+
+
+  // w500
+  // http://image.tmdb.org/t/p/
+}
+
+function envVars(){
+  var url = 'https://api.themoviedb.org/',
+  api_key, read_access;
+  return {
+    set_api_key: function(data){
+      api_key = data;
+    },
+    set_read_access: function(data){
+      read_access = data;
+    },
+    get_api_key: function(){
+      return api_key;
+    },
+    get_read_access: function(){
+      return read_access;
+    },
+    get_url: function(){
+      return url;
+    }
+  }
+}
